803d1b7131f55d2abffa9790109789512beeb89a
David Rhodes <david@espressocode.tech>
2020-10-27 17:21:38 -0500

    fix(animation): switches to use refs

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const cardRef = useRef(null);
+  const containerRef = useRef(null);
+  const titleRef = useRef(null);
+  const sneakerRef = useRef(null);
+  const purchaseRef = useRef(null);
+  const allHalfSecondEase = `all 0.5s ease`;
+  const container = containerRef.current;
+  const card = cardRef.current;
+  const title = titleRef.current;
+  const sneaker = sneakerRef.current;
+  const purchase = purchaseRef.current;
+
   useLayoutEffect(() => {
-    const card = document.querySelector('.card');
-    const container = document.querySelector('.container');
     const skewCard = (e) => {
       let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
       let yAxis = (window.innerWidth / 2 - e.pageY) / 25;
-      card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+      cardRef.current.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
     };
 
     container.addEventListener('mousemove', skewCard);
     return () => {
       container.removeEventListener('mousemove', skewCard);
     };
-  }, []);
+  }, [container]);
 
   useLayoutEffect(() => {
-    const card = document.querySelector('.card');
-    const container = document.querySelector('.container');
-    const title = document.querySelector('.title');
-    const sneaker = document.querySelector('.sneaker img');
-    const purchase = document.querySelector('.purchase button');
-    const allHalfSecondEase = `all 0.5s ease`;
     const easeInCard = () => {
-      card.style.transition = allHalfSecondEase;
+      cardRef.current.style.transition = allHalfSecondEase;
       title.style.transition = allHalfSecondEase;
       sneaker.style.transition = allHalfSecondEase;
       purchase.style.transition = allHalfSecondEase;
@@ -38,15 +42,15 @@ function App() {
     return () => {
       container.removeEventListener('mouseenter', easeInCard);
     };
-  }, []);
+  }, [
+    allHalfSecondEase,
+    container,
+    purchase.style,
+    sneaker.style,
+    title.style,
+  ]);
 
   useLayoutEffect(() => {
-    const card = document.querySelector('.card');
-    const container = document.querySelector('.container');
-    const title = document.querySelector('.title');
-    const sneaker = document.querySelector('.sneaker img');
-    const purchase = document.querySelector('.purchase button');
-    const allHalfSecondEase = `all 0.5s ease`;
     const easeOutCard = () => {
       card.style.transition = allHalfSecondEase;
       title.style.transition = allHalfSecondEase;
@@ -62,25 +66,34 @@ function App() {
     return () => {
       container.removeEventListener('mouseleave', easeOutCard);
     };
-  }, []);
+  }, [
+    allHalfSecondEase,
+    container,
+    sneaker.style,
+    purchase.style,
+    card.style,
+    title.style,
+  ]);
 
   return (
-    <div className="container">
-      <div className="card">
+    <div className="container" ref={containerRef}>
+      <div className="card" ref={cardRef}>
         <div className="sneaker">
           <div className="circle"></div>
-          <img src="./zero_grand.png" alt="zero grand shoe" />
+          <img ref={sneakerRef} src="./zero_grand.png" alt="zero grand shoe" />
         </div>
         <div className="info">
-          <h1 className="title">ZERO GRAND by COLE HAAN</h1>
+          <h1 ref={titleRef} className="title">
+            ZERO GRAND by COLE HAAN
+          </h1>
           <h3>We #WriteNewRules for footwear. You do it for the world.</h3>
           <div className="sizes">
-            <button>8.5</button>
-            <button className="active">9</button>
+            <button>9</button>
+            <button className="active">9.5</button>
             <button>10</button>
             <button>10.5</button>
           </div>
-          <div className="purchase">
+          <div ref={purchaseRef} className="purchase">
             <button>Buy Now</button>
           </div>
         </div>
